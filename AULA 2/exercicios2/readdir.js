856d13705cc09dcea3f8609015f33805e54c9383
Bruno Leite Gonçales <brunoleitegoncales@gmail.com>
2022-02-10 09:49:37 -0300

    main.

---
+++ AULA 2/exercicios2/readdir.js
@@ -0,0 +1,11 @@
+const fs = require ('fs')
+const readdir = caminho => new Promise ((resolve, reject)=>{
+    fs.readdir (caminho, (err, arquivos)=> {
+        if (err) {
+            reject(err)
+        }else{
+            resolve(arquivos)
+        }
+    })
+})
+readdir("./").then (arquivos => console.log(arquivos))
